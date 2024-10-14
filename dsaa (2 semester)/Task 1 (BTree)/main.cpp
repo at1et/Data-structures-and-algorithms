af40f69353b8c7592217c191d2cbdad532311c61
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2024-10-14 20:44:57 +0900

    Update main.cpp

--- dsaa (2 semester)/Task 1 (BTree)/main.cpp
+++ dsaa (2 semester)/Task 1 (BTree)/main.cpp
@@ -1,26 +1,8 @@
-#include <iostream>
-#include <memory>  // Для использования умных указателей
-
-using namespace std;
-
-template<typename T>
-class TreeNode {
-public:
-    T data;  // Данные узла
-    shared_ptr<TreeNode<T>> left;  // Указатель на левый дочерний узел
-    shared_ptr<TreeNode<T>> right; // Указатель на правый дочерний узел
-
-    // Конструктор
-    TreeNode(T value) : data(value), left(nullptr), right(nullptr) {}
-
-    // Деструктор (shared_ptr автоматически освободит память)
-    ~TreeNode() {
-        cout << "Удаление узла с данными: " << data << endl;
-    }
-};
+#include "TreeModule.h"  // Подключаем модуль с функциями бинарного дерева
 
 int main() {
-    setlocale(LC_ALL,"rus");
+    setlocale(LC_ALL, "rus");
+
     // Создаем узлы дерева
     shared_ptr<TreeNode<int>> root = make_shared<TreeNode<int>>(1);
     root->left = make_shared<TreeNode<int>>(2);
@@ -28,15 +10,23 @@ int main() {
     root->left->left = make_shared<TreeNode<int>>(4);
     root->left->right = make_shared<TreeNode<int>>(5);
 
-    // Визуализируем дерево 
-    cout << "   " << root->data << endl;
-    cout << "  / \\" << endl;
-    cout << " " << root->left->data << "   " << root->right->data << endl;
-    cout << "/ \\" << endl;
-    cout << root->left->left->data << "   " << root->left->right->data << endl;
 
-    // Посмотрим содержимое узла (например, правого потомка корня)
-    cout << "Содержимое правого потомка  " << root->left->right->data << endl;
+    // Проверка строгого бинарного дерева
+    cout << "\nЯвляется ли дерево строгим? "
+        << (isStrictBinaryTree(root) ? "Да" : "Нет") << endl;
+
+    // Проверка завершенного бинарного дерева
+    cout << "Является ли дерево законченным? "
+        << (isCompleteBinaryTree(root) ? "Да" : "Нет") << endl;
+
+    // Проверка совершенного бинарного дерева
+    int depth = findDepth(root);
+    cout << "Является ли дерево совершенным? "
+        << (isPerfectBinaryTree(root, depth) ? "Да" : "Нет") << endl;
+
+    // Смотрим содержимое одного из узлов (например, правого потомка корня)
+    cout << "\nСодержимое правого потомка корня: "
+        << root->right->data << endl;
 
     // Удаляем все узлы (shared_ptr автоматически очищает память)
     root.reset();
