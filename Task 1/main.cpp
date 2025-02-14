8409ef9af2beabef439aab4f4af2eb1d26e0a707
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2025-02-14 22:44:27 +0900

    Update main.cpp

--- Task 1/main.cpp
+++ Task 1/main.cpp
@@ -2,7 +2,7 @@
 // Author: Полуэктов Андрей
 //
 // Задание 1. Подготовительный код для работы с массивами, измерения времени; последовательный поиск.
-// Ссылка на задание: https://github.com/ivtipm/Data-structures-and-algorithms/blob/main/current_plans/2024/2024_part_1.md
+// Ссылка на задание: https://github.com/ivtipm/Data-structures-and-algorithms/blob/main/current_plans/2024/2024_part_1.md#%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-1-%D0%BF%D0%BE%D0%B4%D0%B3%D0%BE%D1%82%D0%BE%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%BA%D0%BE%D0%B4-%D0%B4%D0%BB%D1%8F-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B-%D1%81-%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B0%D0%BC%D0%B8-%D0%B8%D0%B7%D0%BC%D0%B5%D1%80%D0%B5%D0%BD%D0%B8%D1%8F-%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%B8-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%BF%D0%BE%D0%B8%D1%81%D0%BA
 //
 // Данная программа демонстрирует работу с классическими массивами, включает функции генерации
 // случайных и монотонно возрастающих массивов, последовательный поиск (с вариантами) и измерение времени работы.
